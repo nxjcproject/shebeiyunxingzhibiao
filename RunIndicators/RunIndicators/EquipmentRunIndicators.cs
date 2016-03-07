188dbc172bfc281513b3d9a48aa4143257eb1db5
weapons1990 <286582326@qq.com>
2016-03-07 09:06:56 +0800

    亮哥建立设备运行指标库

---
+++ RunIndicators/RunIndicators/EquipmentRunIndicators.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using SqlServerDataAdapter;
+namespace RunIndicators
+{
+    public class EquipmentRunIndicators
+    {
+        /// <summary>
+        /// 设备可靠性计算方法
+        /// </summary>
+        /// <param name="myCalendarTime">日历时间</param>
+        /// <param name="myRepairTime">检修时间</param>
+        /// <param name="NormalStopTime">正常停机时间</param>
+        /// <param name="myDownTime">故障停机</param>
+        /// <returns>可靠性</returns>
+        private static decimal GetReliability(decimal myCalendarTime, decimal myRepairTime, decimal NormalStopTime, decimal myDownTime)
+        {
+            if (myCalendarTime - myRepairTime - NormalStopTime != 0)
+            {
+                decimal m_Reliability = (myCalendarTime - myRepairTime - NormalStopTime - myDownTime) / (myCalendarTime - myRepairTime - NormalStopTime);
+                return m_Reliability;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        /// <summary>
+        /// 设备运转率计算方法
+        /// </summary>
+        /// <param name="myCalendarTime">日历时间</param>
+        /// <param name="myRepairTime">检修时间</param>
+        /// <param name="NormalStopTime">正常停机时间</param>
+        /// <param name="myDownTime">故障停机</param>
+        /// <returns>运转率</returns>
+        private static decimal GetRunningRate(decimal myCalendarTime, decimal myRepairTime, decimal NormalStopTime, decimal myDownTime)
+        {
+            decimal m_RunningRate = (myCalendarTime - myRepairTime - NormalStopTime - myDownTime) / myCalendarTime;
+            return m_RunningRate;
+        }
+        /// <summary>
+        /// 设备故障率计算方法
+        /// </summary>
+        /// <param name="myCalendarTime">日历时间</param>
+        /// <param name="myRepairTime">检修时间</param>
+        /// <param name="NormalStopTime">正常停机时间</param>
+        /// <param name="myDownTime">故障停机</param>
+        /// <returns>设备故障率</returns>
+        private static decimal GetFailureRate(decimal myCalendarTime, decimal myRepairTime, decimal NormalStopTime, decimal myDownTime)
+        {
+            if (myCalendarTime - myRepairTime - NormalStopTime != 0)
+            {
+                decimal m_FailureRate = myDownTime / (myCalendarTime - myRepairTime - NormalStopTime);
+                return m_FailureRate;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public static DataTable GetResultDataTable()
+        {
+            DataTable m_ResultDataTable = new DataTable();
+            m_ResultDataTable.Columns.Add("EquipmentId",typeof(string));
+            m_ResultDataTable.Columns.Add("January", typeof(decimal));
+            m_ResultDataTable.Columns.Add("February", typeof(decimal));
+            m_ResultDataTable.Columns.Add("March", typeof(decimal));
+            m_ResultDataTable.Columns.Add("April", typeof(decimal));
+            m_ResultDataTable.Columns.Add("May", typeof(decimal));
+            m_ResultDataTable.Columns.Add("June", typeof(decimal));
+            m_ResultDataTable.Columns.Add("July", typeof(decimal));
+            m_ResultDataTable.Columns.Add("August", typeof(decimal));
+            m_ResultDataTable.Columns.Add("September", typeof(decimal));
+            m_ResultDataTable.Columns.Add("October", typeof(decimal));
+            m_ResultDataTable.Columns.Add("November", typeof(decimal));
+            m_ResultDataTable.Columns.Add("December", typeof(decimal));
+            return m_ResultDataTable;
+        }
+        public static DataTable GetEquipmentUtilizationPerMonth(string myProductionQuotasId, string myOrganizationId, string myPlanYear, string myEquipmentCommonId, ISqlServerDataFactory myDataFactory)
+        {
+            string m_Sql = @"Select M.EquipmentID as EquipmentId, M.ReasonID as ReasonId, convert(varchar(7),M.HaltTimeF,20) as HaltTimeMonthF, sum(M.HaltLong) as Value
+                                from (SELECT A.MachineHaltLogID
+                                      ,B.OrganizationID
+                                      ,A.EquipmentID
+	                                  ,A.HaltTime
+	                                  ,A.RecoverTime
+	                                  ,substring(A.ReasonID,1,3) as ReasonID
+	                                  ,(case when convert(varchar(7),A.HaltTime,20) < D.PreMonth then  CONVERT(datetime,D.PreMonth + '-01 00:00:00') else A.HaltTime end) as HaltTimeF
+	                                  ,(case when convert(varchar(7),A.RecoverTime,20) > D.PreMonth then dateadd(day,-1, dateadd(month, 1, CONVERT(datetime,D.PreMonth + '-01 23:59:59'))) else A.RecoverTime end) as RecoverTimeF
+	                                  ,DATEDIFF (hour, (case when convert(varchar(7),A.HaltTime,20) < D.PreMonth then  CONVERT(datetime,D.PreMonth + '-01 00:00:00') else A.HaltTime end)
+	                                  ,(case when convert(varchar(7),A.RecoverTime,20) > D.PreMonth then dateadd(day,-1, dateadd(month, 1, CONVERT(datetime,D.PreMonth + '-01 23:59:59'))) else A.RecoverTime end)) as HaltLong
+                                  FROM shift_MachineHaltLog A, system_Organization B, system_Organization C, 
+                                  (select convert(varchar(7),dateadd(month,number,@StartTime),120) as PreMonth
+                                      from master..spt_values
+                                      where type='P'
+                                      and dateadd(month,number,@StartTime)<=@EndTime) D, equipment_EquipmentDetail E
+                                  where A.HaltTime >= @StartTime
+                                  and A.HaltTime <= @EndTime
+                                  and (A.ReasonID like 'E01%' or A.ReasonID like 'E02%')
+                                  and B.OrganizationID = @OrganizationID
+                                  and C.LevelCode like B.LevelCode + '%'
+                                  and A.OrganizationID = C.OrganizationID
+                                  and convert(varchar(7),A.HaltTime,20) <= D.PreMonth and convert(varchar(7),RecoverTime,20) >= D.PreMonth
+                                  and B.OrganizationID = E.OrganizationID
+                                  and E.EquipmentCommonId = @EquipmentCommonId
+                                  and A.EquipmentID = E.EquipmentID) M
+                                  group by M.EquipmentID, M.ReasonID, convert(varchar(7),M.HaltTimeF,20)
+                                  order by B.OrganizationID, A.EquipmentID, convert(varchar(7),M.HaltTimeF,20), ReasonID";
+            try
+            {
+                SqlParameter[] m_Parameters = { new SqlParameter("@OrganizationID", myOrganizationId)
+                                              , new SqlParameter("@EquipmentCommonId", myEquipmentCommonId)
+                                              , new SqlParameter("@StartTime", myPlanYear + "-01-01")
+                                              , new SqlParameter("@EndTime", myPlanYear + "-12-31")};
+                DataTable m_Result = myDataFactory.Query(m_Sql, m_Parameters);
+                if (m_Result != null)
+                {
+                    DataTable m_ResultDataTable = GetResultDataTable();
+                    string m_EquipmentId = "";
+                    decimal m_NormalStopTime = 0.0m;   //正常停机(小时)
+                    decimal m_DownTime = 0.0m;         //故障停机时间(小时)
+                    decimal m_RepairTime = 0.0m;
+                    decimal m_CalendarTime = 0.0m;
+                    int m_MonthIndex = 0;
+                    for (int i = 0; i < m_Result.Rows.Count; i++)
+                    {
+                        if (m_EquipmentId != m_Result.Rows[i]["EquipmentId"].ToString())      //如果是不同设备,需要另起一行
+                        {
+                            if (i != 0)           //如果不是第一次进入,则计算一次
+                            {
+                                if (myProductionQuotasId.Contains("运转率"))
+                                {
+                                    m_ResultDataTable.Rows[m_ResultDataTable.Rows.Count - 1][m_MonthIndex] = GetRunningRate(m_CalendarTime, m_RepairTime, m_NormalStopTime, m_DownTime);
+                                }
+                                else if (myProductionQuotasId.Contains("运转时间"))
+                                {
+                                    m_ResultDataTable.Rows[m_ResultDataTable.Rows.Count - 1][m_MonthIndex] = m_CalendarTime - m_RepairTime - m_NormalStopTime - m_DownTime;
+                                }
+                            }
+                            DataRow m_NewRow = m_ResultDataTable.NewRow();
+                            m_EquipmentId = m_Result.Rows[i]["EquipmentId"].ToString();
+                            m_NewRow[0] = m_EquipmentId;
+
+                            m_NormalStopTime = 0;
+                            m_DownTime = 0;
+                            m_RepairTime = 0;
+
+                            DateTime m_MonthStart = DateTime.Parse(m_Result.Rows[i]["HaltTimeMonthF"].ToString() + "-01");
+                            m_MonthIndex = m_MonthStart.Month;
+                            m_CalendarTime = (m_MonthStart.AddMonths(1) - m_MonthStart).Hours;
+                            if (m_Result.Rows[i]["ReasonId"].ToString() == "E01")
+                            {
+                                m_NormalStopTime = decimal.Parse(m_Result.Rows[i]["Value"].ToString());
+                            }
+                            else if (m_Result.Rows[i]["ReasonId"].ToString() == "E02")
+                            {
+                                m_DownTime = decimal.Parse(m_Result.Rows[i]["Value"].ToString());
+                            }
+                            m_Result.Rows.Add(m_NewRow);
+                        }
+                        else
+                        {
+                            if (DateTime.Parse(m_Result.Rows[i]["HaltTimeMonthF"].ToString() + "-01").Month != m_MonthIndex)   //如果是相同设备不同月份,需要计算,填入相应的月份中
+                            {
+                                if (myProductionQuotasId.Contains("运转率"))
+                                {
+                                    m_ResultDataTable.Rows[m_ResultDataTable.Rows.Count - 1][m_MonthIndex] = GetRunningRate(m_CalendarTime, m_RepairTime, m_NormalStopTime, m_DownTime);
+                                }
+                                else if (myProductionQuotasId.Contains("运转时间"))
+                                {
+                                    m_ResultDataTable.Rows[m_ResultDataTable.Rows.Count - 1][m_MonthIndex] = m_CalendarTime - m_RepairTime - m_NormalStopTime - m_DownTime;
+                                }
+                                DateTime m_MonthStart = DateTime.Parse(m_Result.Rows[i]["HaltTimeMonthF"].ToString() + "-01");
+                                m_MonthIndex = m_MonthStart.Month;
+                                m_CalendarTime = (m_MonthStart.AddMonths(1) - m_MonthStart).Hours;
+
+                                m_NormalStopTime = 0;
+                                m_DownTime = 0;
+                                m_RepairTime = 0;
+                            }
+                            if (m_Result.Rows[i]["ReasonId"].ToString() == "E01")
+                            {
+                                m_NormalStopTime = decimal.Parse(m_Result.Rows[i]["Value"].ToString());
+                            }
+                            else if (m_Result.Rows[i]["ReasonId"].ToString() == "E02")
+                            {
+                                m_DownTime = decimal.Parse(m_Result.Rows[i]["Value"].ToString());
+                            }
+                        }
+
+                    }
+                    if (myProductionQuotasId.Contains("运转率"))
+                    {
+                        m_ResultDataTable.Rows[m_ResultDataTable.Rows.Count - 1][m_MonthIndex] = GetRunningRate(m_CalendarTime, m_RepairTime, m_NormalStopTime, m_DownTime);
+                    }
+                    else if (myProductionQuotasId.Contains("运转时间"))
+                    {
+                        m_ResultDataTable.Rows[m_ResultDataTable.Rows.Count - 1][m_MonthIndex] = m_CalendarTime - m_RepairTime - m_NormalStopTime - m_DownTime;
+                    }
+                    else if (myProductionQuotasId.Contains("台时产量"))
+                    {
+                        m_ResultDataTable.Rows[m_ResultDataTable.Rows.Count - 1][m_MonthIndex] = m_CalendarTime - m_RepairTime - m_NormalStopTime - m_DownTime;
+                    }
+                    return m_ResultDataTable;
+                }
+                else
+                {
+                    return null;
+                }          
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ RunIndicators/RunIndicators/MaterialWeightResult.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using SqlServerDataAdapter;
+namespace RunIndicators
+{
+    public class MaterialWeightResult
+    {
+        private const string MaterialWeight = "MaterialWeight";
+        private const string EquipmentUtilization = "EquipmentUtilization";
+        public static DataTable GetMaterialWeightResultPerMonth(string myProductionQuotasId, string myOrganizationId, string myPlanYear, string myEquipmentCommonId, ISqlServerDataFactory myDataFactory)
+        {
+            string m_Sql = @"Select B.EquipmentName + A.QuotasName as VariableName, B.EquipmentId, A.QuotasID, C.TimeStamp, sum(D.TotalPeakValleyFlatB) as Value
+                                from plan_ProductionPlan_Template A, equipment_EquipmentDetail B, tz_Balance C, balance_Production D
+                                where B.OrganizationID = @OrganizationId
+                                and B.EquipmentCommonId = @EquipmentCommonId
+                                and A.EquipmentCommonId = B.EquipmentCommonId
+                                and A.QuotasID = @QuotasID
+                                and C.StaticsCycle = 'month'
+                                and C.TimeStamp >= @StartTime
+                                and C.TimeStamp <= @EndTime
+                                and B.OrganizationID = C.OrganizationID
+                                and C.BalanceId = D.KeyId
+                                and D.VariableId = B.EquipmentId + A.QuotasID
+                                group by B.OrganizationID, B.EquipmentId, A.QuotasID, B.EquipmentName + A.QuotasName, C.TimeStamp
+                                order by B.EquipmentId, A.QuotasID, C.TimeStamp";
+            try
+            {
+                SqlParameter[] m_Parameters = { new SqlParameter("@OrganizationID", myOrganizationId)
+                                              , new SqlParameter("@EquipmentCommonId", myEquipmentCommonId)
+                                              , new SqlParameter("@QuotasID", myProductionQuotasId)
+                                              , new SqlParameter("@StartTime", myPlanYear + "-01")
+                                              , new SqlParameter("@EndTime", myPlanYear + "-12")};
+                DataTable m_Result = myDataFactory.Query(m_Sql, m_Parameters);
+                if (m_Result != null)
+                {
+                    DataTable m_ResultDataTable = EquipmentRunIndicators.GetResultDataTable();
+                    string m_EquipmentId = "";
+                    for (int i = 0; i < m_Result.Rows.Count; i++)
+                    {
+                        if (m_EquipmentId != m_Result.Rows[i]["EquipmentId"].ToString())
+                        {
+                            DataRow m_NewRow = m_ResultDataTable.NewRow();
+                            m_NewRow[0] = m_EquipmentId;
+                            int m_MonthIndex = DateTime.Parse(m_Result.Rows[i]["TimeStamp"].ToString() + "-01").Month;
+                            m_NewRow[m_MonthIndex] = decimal.Parse(m_Result.Rows[i]["Value"].ToString());
+                            m_Result.Rows.Add(m_NewRow);
+                        }
+                        else
+                        {
+                            int m_MonthIndex = DateTime.Parse(m_Result.Rows[i]["TimeStamp"].ToString() + "-01").Month;
+                            m_ResultDataTable.Rows[m_ResultDataTable.Rows.Count - 1][m_MonthIndex] = decimal.Parse(m_Result.Rows[i]["Value"].ToString());
+                        }
+                    }
+                    return m_ResultDataTable;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+        public static DataTable GetMaterialWeightResultByDenominatorPerMonth(string myProductionQuotasId, string myOrganizationId, string myPlanYear, string myEquipmentCommonId, ISqlServerDataFactory myDataFactory)
+        {
+            string m_Sql = @"Select B.EquipmentName + A.QuotasName as VariableName, B.EquipmentId, A.QuotasID, C.TimeStamp, sum(D.TotalPeakValleyFlatB) as Value
+                                from plan_ProductionPlan_Template A, equipment_EquipmentDetail B, tz_Balance C, balance_Production D, plan_ProductionPlan_Template E
+                                where B.OrganizationID = @OrganizationId
+                                and B.EquipmentCommonId = @EquipmentCommonId
+                                and A.EquipmentCommonId = B.EquipmentCommonId
+                                and A.QuotasID = E.Denominator
+                                and E.QuotasID = @QuotasID
+                                and C.StaticsCycle = 'month'
+                                and C.TimeStamp >= @StartTime
+                                and C.TimeStamp <= @EndTime
+                                and B.OrganizationID = C.OrganizationID
+                                and C.BalanceId = D.KeyId
+                                and D.VariableId = B.EquipmentId + A.QuotasID
+                                group by B.OrganizationID, B.EquipmentId, A.QuotasID, B.EquipmentName + A.QuotasName, C.TimeStamp
+                                order by B.EquipmentId, A.QuotasID, C.TimeStamp";
+            try
+            {
+                SqlParameter[] m_Parameters = { new SqlParameter("@OrganizationID", myOrganizationId)
+                                              , new SqlParameter("@EquipmentCommonId", myEquipmentCommonId)
+                                              , new SqlParameter("@QuotasID", myProductionQuotasId)
+                                              , new SqlParameter("@StartTime", myPlanYear + "-01")
+                                              , new SqlParameter("@EndTime", myPlanYear + "-12")};
+                DataTable m_Result = myDataFactory.Query(m_Sql, m_Parameters);
+                if (m_Result != null)
+                {
+                    DataTable m_ResultDataTable = EquipmentRunIndicators.GetResultDataTable();
+                    string m_EquipmentId = "";
+                    for (int i = 0; i < m_Result.Rows.Count; i++)
+                    {
+                        if (m_EquipmentId != m_Result.Rows[i]["EquipmentId"].ToString())
+                        {
+                            DataRow m_NewRow = m_ResultDataTable.NewRow();
+                            m_NewRow[0] = m_EquipmentId;
+                            int m_MonthIndex = DateTime.Parse(m_Result.Rows[i]["TimeStamp"].ToString() + "-01").Month;
+                            m_NewRow[m_MonthIndex] = decimal.Parse(m_Result.Rows[i]["Value"].ToString());
+                            m_Result.Rows.Add(m_NewRow);
+                        }
+                        else
+                        {
+                            int m_MonthIndex = DateTime.Parse(m_Result.Rows[i]["TimeStamp"].ToString() + "-01").Month;
+                            m_ResultDataTable.Rows[m_ResultDataTable.Rows.Count - 1][m_MonthIndex] = decimal.Parse(m_Result.Rows[i]["Value"].ToString());
+                        }
+                    }
+                    return m_ResultDataTable;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
